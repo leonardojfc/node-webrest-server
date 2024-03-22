f639827f4c927e8e64ff434dfbf3bbccc750fa2e
ljferreyrac <lferreyrac04@hotmail.com>
2024-03-22 18:44:26 -0500

    CRUD added

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -10,22 +11,29 @@ export class Server {
     
     private app = express();
     private readonly port: number;
+    private readonly routes: Router;
     private readonly publicPath: string;
 
     constructor(options: Options){
-        const { port, public_path = 'public' } = options;
+        const { port, public_path = 'public', routes } = options;
         this.port = port;
         this.publicPath = public_path
+        this.routes = routes
     }
 
     async start() {
 
         // Middlewares
+        this.app.use( express.json() );
+        this.app.use( express.urlencoded({extended: true}));
 
         // Public Folder
-
         this.app.use( express.static(this.publicPath) );
 
+        // Routes
+        this.app.use(this.routes);
+
+        // SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
