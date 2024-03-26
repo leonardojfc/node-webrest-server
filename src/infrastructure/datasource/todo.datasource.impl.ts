7ff051b30a887b6417b950dc9fe2b749cdaf7c5d
ljferreyrac <lferreyrac04@hotmail.com>
2024-03-26 12:13:44 -0500

    Test added

--- src/infrastructure/datasource/todo.datasource.impl.ts
+++ src/infrastructure/datasource/todo.datasource.impl.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../../data/postgres";
-import { CreateTodoDTO, TodoDatasource, TodoEntity, UpdateTodoDTO } from "../../domain";
+import { CreateTodoDTO, CustomError, TodoDatasource, TodoEntity, UpdateTodoDTO } from "../../domain";
 
 export class TodoDatasourceImpl implements TodoDatasource{
 
@@ -20,7 +20,7 @@ export class TodoDatasourceImpl implements TodoDatasource{
             where: {id}
         });
 
-        if( !todo ) throw `Todo with ${id} not found`
+        if( !todo ) throw new CustomError(`Todo with id ${id} not found`, 404);
         return TodoEntity.fromObject(todo);
     }
     async updateById(updateTodoDTO: UpdateTodoDTO): Promise<TodoEntity> {
